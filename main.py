9c6067ba48af1106e7e7f56afaa74cede6826c73
Christopher Bertrand <c.bertrandtjo@gmail.com>
2025-07-29 10:02:13 +0800

    Initial commit: basic screen and sprite drawn

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+os.environ["SDL_AUDIODRIVER"] = "dummy"
+
+import pygame
+from constants import *
+from player import *
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	running = True
+	clock = pygame.time.Clock()
+	dt = 0
+	player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+	while running:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				running = False
+		screen.fill((0, 0, 0))
+		player.draw(screen)
+		pygame.display.flip()
+
+		dt = clock.tick(60)/1000
+	pygame.quit()
+if __name__ == "__main__":
+	main()
